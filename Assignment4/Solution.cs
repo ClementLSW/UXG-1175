b8c3bc90302a63fd9cfe9d7350050d2ade46b8b4
Clement Leow <clementlsw@gmail.com>
2023-02-05 18:31:53 +0800

    Added Comments to Solution.cs

--- Assignment4/Solution.cs
+++ Assignment4/Solution.cs
@@ -22,28 +22,28 @@ partial class Program {
     }
 
     static bool Search<K, V>(Node<K, V> root, K keyToSearch, ref V valOutput) where K : IComparable<K> {
-        // TODO
+        // If end of tree
         if (root == null) {
-            return false;
+            return false;                                   // Return False; i.e. to say: no such entry
         }
-        else if (keyToSearch.CompareTo(root.key) != 0) {
+        else if (keyToSearch.CompareTo(root.key) != 0) {    // If current root key is not result we are looking for
             //Console.Write("_Debug_: NodeKey: {0}, KeyToSearch: {1}\n", root.key, keyToSearch);
-            if (keyToSearch.CompareTo(root.key) > 0) {
+            if (keyToSearch.CompareTo(root.key) > 0) {      // If result we want is bigger than current root key, search right branch
                 return Search(root.right, keyToSearch, ref valOutput);
             }
-            else if (keyToSearch.CompareTo(root.key) < 0) {
+            else if (keyToSearch.CompareTo(root.key) < 0) { // If result we want is smaller than current root key, search left branch
                 return Search(root.left, keyToSearch, ref valOutput);
             }
         }
-        else{
-            valOutput = root.val;
-            return true;
+        else{                                               // If current root key is the answer
+            valOutput = root.val;                           // Set ref ValOutput to associated value
+            return true;                                    // Return true to denote result found
         }
 
-        return false;
+        return false;                                       // Catch-all Return False [Should not ever reach here]
     }
     static void Modify<K, V>(Node<K, V> root, K keyToSearch, V newValue) where K : IComparable<K> {
-        // TODO
+        // If cannot find required key, just return
         if (root == null) {
             return;
         }
@@ -60,26 +60,26 @@ partial class Program {
     }
 
     static void PrintPreOrder<K, V>(Node<K, V> root) where K : IComparable<K> {
-        // TODO
+        // If end of tree
         if (root == null) {
             return;
         }
         else {
-            Console.Write("[{0} = {1}]", root.key, root.val);
-            PrintPreOrder(root.left);
-            PrintPreOrder(root.right);
+            Console.Write("[{0} = {1}]", root.key, root.val);       // Step 1: Visit Node
+            PrintPreOrder(root.left);                               // Step 2: Visit left branch
+            PrintPreOrder(root.right);                              // Step 3: Visit right branch
         }
     }
 
     static void PrintPostOrder<K, V>(Node<K, V> root) where K : IComparable<K> {
-        // TODO
+        // If end of tree
         if (root == null) {
             return;
         }
         else {
-            PrintPostOrder(root.left);
-            PrintPostOrder(root.right);
-            Console.Write("[{0} = {1}]", root.key, root.val);
+            PrintPostOrder(root.left);                              // Step 1: Visit left branch
+            PrintPostOrder(root.right);                             // Step 2: Visit right branch
+            Console.Write("[{0} = {1}]", root.key, root.val);       // Step 3: Visit node
         }
     }
 }
